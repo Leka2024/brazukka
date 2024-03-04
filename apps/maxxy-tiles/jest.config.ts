550d33305aea4a0f3d2d465d747e309c5a5fb8c2
Johan Tuitel <jtuitel@hotmail.com>
2024-03-04 21:21:54 +0100

    try to fix build

--- apps/maxxy-tiles/jest.config.ts
+++ apps/maxxy-tiles/jest.config.ts
@@ -4,6 +4,15 @@ export default {
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   coverageDirectory: '../../coverage/apps/maxxy-tiles',
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': [
+      '<root-dir>/node_modules/jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
+  },
   transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
   snapshotSerializers: [
     'jest-preset-angular/build/serializers/no-ng-attributes',
--- libs/ui/jest.config.ts
+++ libs/ui/jest.config.ts
@@ -4,15 +4,15 @@ export default {
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   coverageDirectory: '../../coverage/libs/ui',
-  // transform: {
-  //   '^.+\\.(ts|mjs|js|html)$': [
-  //     'jest-preset-angular',
-  //     {
-  //       tsconfig: '<rootDir>/tsconfig.spec.json',
-  //       stringifyContentPathRegex: '\\.(html|svg)$',
-  //     },
-  //   ],
-  // },
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': [
+      '<root-dir>/node_modules/jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
+  },
   transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
   snapshotSerializers: [
     'jest-preset-angular/build/serializers/no-ng-attributes',
